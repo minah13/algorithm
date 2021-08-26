f31d9f72c99d81aeb307464f32152a67c2db837a
minah <50553183+minah13@users.noreply.github.com>
2021-08-26 17:08:12 +0900

    시간복잡도 개선

--- exam_16197.java
+++ exam_16197.java
@@ -26,11 +26,6 @@ public class exam_16197 {
 			String tmp = br.readLine();
 			for(int j=0;j<M;j++) {
 				board[i][j] = tmp.charAt(j);
-			}
-		}
-		
-		for(int i =0;i<N;i++) {
-			for(int j=0;j<M;j++) {
 				if(board[i][j] == 'o') {
 					if(x1==-1) {
 						x1 = i;
@@ -40,13 +35,13 @@ public class exam_16197 {
 						y2 = j;
 							
 					}
+					board[i][j] = '.';
 				}
 			}
 		}
 		
-		for(int i =0;i<4;i++) {
-			move(board, 1,x1,y1,x2,y2,i);
-		}
+		move(0,x1,y1,x2,y2);
+		
 		if(min >10) {
 			System.out.println(-1);
 		}
@@ -56,62 +51,42 @@ public class exam_16197 {
 		
 	}
 	
-	public static void move(char[][] b, int count, int x1, int y1, int x2, int y2, int dir) {
+	public static void move(int count, int x1, int y1, int x2, int y2) {
 		if(count>10) {
-			return;
+			return ;
 		}
-		int x1_move = x1 + x_dir[dir];
-		int x2_move = x2 + x_dir[dir];
-		int y1_move = y1 + y_dir[dir];
-		int y2_move = y2 + y_dir[dir];
+		boolean x1y1 = false;
+		boolean x2y2 = false;
+		if(x1>=N || x1<0 || y1>=M || y1<0) x1y1=true;
+		if(x2>=N || x2<0 || y2>=M || y2<0) x2y2=true;
 		
-		char[][] tmp = new char[N][M];
-		for(int i =0;i<N;i++) {
-			for(int j =0; j<M;j++) {
-				tmp[i][j] = b[i][j];
-			}
-		}
-		
-		if(x1_move>=N || x1_move<0 || y1_move>=M || y1_move<0) {
-			if(x2_move>=0 && x2_move<N && y2_move>=0 && y2_move<M) {
-				if(min>count) {
-					min = count;
-				}
-			}
-			return;
-		}
-		if(x2_move>=N || x2_move<0 || y2_move>=M || y2_move<0) {
-			if(x1_move>=0 && x1_move<N && y1_move>=0 && y1_move<M) {
-				if(min>count) {
-					min = count;
-				}
-			}
+		if(x1y1 && x2y2) return;
+		if(x1y1 || x2y2) {
+			if(min>count)
+				min = count;
 			return;
 		}
 		
-		if(tmp[x1_move][y1_move] == '.' || tmp[x1_move][y1_move] == 'o') {
-			tmp[x1][y1] = '.';
+		
+		for(int i =0;i<4;i++) {
+			int x1_move = x1 + x_dir[i];
+			int x2_move = x2 + x_dir[i];
+			int y1_move = y1 + y_dir[i];
+			int y2_move = y2 + y_dir[i];
 			
-		}
-		else if(tmp[x1_move][y1_move] == '#') {
-			x1_move = x1;
-			y1_move = y1;
-		}
-		if(tmp[x2_move][y2_move] == '.' || tmp[x2_move][y2_move] == 'o') {
-			tmp[x2][y2] = '.';
+			if(x1_move>=0 && x1_move<N && y1_move>=0 && y1_move<M && board[x1_move][y1_move] == '#') {
+				x1_move = x1;
+				y1_move = y1;
+			}
 			
-		}
-		else if(tmp[x2_move][y2_move] == '#') {
-			x2_move = x2;
-			y2_move = y2;
+			if(x2_move>=0 && x2_move<N && y2_move>=0 && y2_move<M && board[x2_move][y2_move] == '#') {
+				x2_move = x2;
+				y2_move = y2;
+			}
+			
+			move(count+1,x1_move,y1_move, x2_move, y2_move);
 		}
 		
-		tmp[x1_move][y1_move] = 'o';
-		tmp[x2_move][y2_move] = 'o';
-		
-		for(int i=0;i<4;i++) {
-			move(tmp,count+1,x1_move,y1_move,x2_move,y2_move,i);
-		}
 		
 	}
 }
