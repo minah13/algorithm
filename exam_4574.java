c7c1959db151ac181bd37658dda89b43b989f126
minah <50553183+minah13@users.noreply.github.com>
2021-08-31 12:59:26 +0900

    Update exam_4574.java

--- exam_4574.java
+++ exam_4574.java
@@ -12,6 +12,7 @@ public class exam_4574 {
 	static boolean[][] column;
 	static boolean[][] square;
 	static boolean end;
+	final static int n=9;
 	
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -21,11 +22,12 @@ public class exam_4574 {
 			if(N==0)
 				break;
 			turn++;
-			int not_used = 36;
+			
 			int first;
 			int[] first_index = new int[2];
 			int second;
 			int[] second_index = new int[2];
+			int not_used = 36;
 			
 			puzzle = new int[9][9];
 			domino = new boolean[10][10];
@@ -44,25 +46,21 @@ public class exam_4574 {
 				String tmp = st.nextToken();
 				first_index[0] = (int)tmp.charAt(0) - (int)'A';
 				first_index[1] = Character.getNumericValue(tmp.charAt(1))-1;
-				//System.out.println(first_index[0]);
-				//System.out.println(first_index[1]);
+				
 				second = Integer.parseInt(st.nextToken());
 				tmp = st.nextToken();
 				second_index[0] = (int)tmp.charAt(0) - (int)'A';
 				second_index[1] = Character.getNumericValue(tmp.charAt(1))-1;
-				//System.out.println(second_index[0]);
-				//System.out.println(second_index[1]);
+				
 				domino[first][second] = true;
 				domino[second][first] = true;
-				not_used--;
+				
 				puzzle[first_index[0]][first_index[1]] = first;
 				puzzle[second_index[0]][second_index[1]] = second;
-				row[first_index[0]][first] = true;
-				column[first_index[1]][first] = true;
-				square[first_index[0]/3*3 + first_index[1]/3][first] = true;
-				row[second_index[0]][second] = true;
-				column[second_index[1]][second] = true;
-				square[second_index[0]/3*3 + second_index[1]/3][second] = true;
+				
+				change(first_index[0],first_index[1],first,true);
+				change(second_index[0],second_index[1],second,true);
+				not_used--;
 			}
 			
 			StringTokenizer st = new StringTokenizer(br.readLine());
@@ -83,9 +81,17 @@ public class exam_4574 {
 		bw.close();
 	}
 	
+	static void change(int x, int y, int num, boolean tf) {
+		row[x][num] = tf;
+		column[y][num] = tf;
+		square[cal_squ(x,y)][num] = tf;
+	}
+	
+	static int cal_squ(int x, int y) {
+		return x/3*3 + y/3;
+	}
+	
 	public static void insert(BufferedWriter bw,int x, int y,int not_used) throws Exception{
-		if(x==9)
-			return;
 		if(not_used == 0) {
 			bw.write("Puzzle " + turn + "\n");
 			for(int i=0;i<9;i++) {
@@ -97,6 +103,9 @@ public class exam_4574 {
 			end = true;
 			return;
 		}
+		if(x==9)
+			return;
+		
 		int empty_x=-1;
 		int empty_y=-1;
 		
@@ -131,12 +140,9 @@ public class exam_4574 {
 							domino[j][i] = true;
 							puzzle[empty_x][empty_y] = i;
 							puzzle[empty_x][empty_y+1] = j;
-							row[empty_x][i] = true;
-							column[empty_y][i] = true;
-							square[empty_x/3*3 + empty_y/3][i] = true;
-							row[empty_x][j] = true;
-							column[empty_y+1][j] = true;
-							square[empty_x/3*3 + (empty_y+1)/3][j] = true;
+							change(empty_x,empty_y,i,true);
+							change(empty_x,empty_y+1,j,true);
+							
 							if(empty_y+2<9) {
 								insert(bw,empty_x,empty_y+2,not_used-1);
 								if(end == true)
@@ -150,12 +156,8 @@ public class exam_4574 {
 							domino[j][i] = false;
 							puzzle[empty_x][empty_y] = 0;
 							puzzle[empty_x][empty_y+1] = 0;
-							row[empty_x][i] = false;
-							column[empty_y][i] = false;
-							square[empty_x/3*3 + empty_y/3][i] = false;
-							row[empty_x][j] = false;
-							column[empty_y+1][j] = false;
-							square[empty_x/3*3 + (empty_y+1)/3][j] = false;
+							change(empty_x,empty_y,i,false);
+							change(empty_x,empty_y+1,j,false);
 						}
 					}
 				}
@@ -167,12 +169,9 @@ public class exam_4574 {
 							domino[j][i] = true;
 							puzzle[empty_x][empty_y] = i;
 							puzzle[empty_x+1][empty_y] = j;
-							row[empty_x][i] = true;
-							column[empty_y][i] = true;
-							square[empty_x/3*3 + empty_y/3][i] = true;
-							row[empty_x+1][j] = true;
-							column[empty_y][j] = true;
-							square[(empty_x+1)/3*3 + empty_y/3][j] = true;
+							change(empty_x,empty_y,i,true);
+							change(empty_x+1,empty_y,j,true);
+							
 							if(empty_y+1<9) {
 								insert(bw,empty_x,empty_y+1,not_used-1);
 								if(end)
@@ -186,12 +185,9 @@ public class exam_4574 {
 							domino[j][i] = false;
 							puzzle[empty_x][empty_y] = 0;
 							puzzle[empty_x+1][empty_y] = 0;
-							row[empty_x][i] = false;
-							column[empty_y][i] = false;
-							square[empty_x/3*3 + empty_y/3][i] = false;
-							row[empty_x+1][j] = false;
-							column[empty_y][j] = false;
-							square[(empty_x+1)/3*3 + empty_y/3][j] = false;
+							change(empty_x,empty_y,i,false);
+							change(empty_x+1,empty_y,j,false);
+							
 						}
 					}
 				}
