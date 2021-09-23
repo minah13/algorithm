6a0ec2371f89f8e9e5a790aebb387c15adf00bb3
minah <50553183+minah13@users.noreply.github.com>
2021-09-23 16:27:52 +0900

    Create exam_10971.java

---
+++ exam_10971.java
@@ -0,0 +1,53 @@
+//10971번 문제(외판원 순회2)
+
+import java.io.*;
+import java.util.*;
+
+public class exam_10971 {
+	static long minW = Long.MAX_VALUE;
+	
+	public static void travel(int N, int[][] W, int index,int count,int w, boolean[] check,int start) {
+		if(count == N-1) {
+			if(W[index][start] == 0)
+				return ;
+			int tmp = w;
+			tmp += W[index][start];
+			if(minW>tmp)
+				minW = tmp;
+			return ;
+		}
+		
+		for(int i=0;i<N;i++) {
+			if(i!=index && check[i]==false && W[index][i] != 0) {
+				check[index] = true;
+				travel(N,W,i,count+1,w+W[index][i],check,start);
+				check[index] = false;
+			}
+		}
+	}
+	
+	public static void main(String args[]) throws Exception{
+		int N;
+		int[][] W;
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		W = new int[N][N];
+		
+		for(int i=0;i<N;i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for(int j=0;j<N;j++) {
+				W[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		
+		//boolean[] check = new boolean[N];
+		//travel(N,W,0,0,0);
+		
+		for(int j=0;j<N;j++) {
+			boolean[] check = new boolean[N];
+			travel(N,W,j,0,0,check,j);
+		}
+		
+		System.out.println(minW);
+	}
+}
